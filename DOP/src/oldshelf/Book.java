e59209f8295057a207a5778c5ff1a62243f38af7
ANUVIND183 <ppanuvind@gmail.com>
2024-10-26 10:38:26 +0530

    Update Book.java

--- DOP/src/oldshelf/Book.java
+++ DOP/src/oldshelf/Book.java
@@ -1,8 +1,9 @@
 package oldshelf;
 
 public abstract class Book {
-
-	public Book() {
+	public String title;
+	public Book(String title) {
 		// TODO: Finish Code here if anything to be done?
+		this.title=title;
 	}
 }
