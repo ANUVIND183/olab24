40d1f4e24c9a1e24024fa5681bccb70fb9da1d6c
ANUVIND183 <ppanuvind@gmail.com>
2024-10-26 10:17:05 +0530

    Update NewSelection.java

--- DOP/src/newshelf/NewSelection.java
+++ DOP/src/newshelf/NewSelection.java
@@ -13,7 +13,18 @@ public class NewSelection {
 	public static String getAgeOrTitle(Object o) {
 		
 		if (o instanceof Book) {
-			
+			if(o instanceof Comic){
+				Comic comic=(Comic) o;
+				return comic.title;
+			}
+			else if(o instanceof Fiction){
+				Fiction fiction=(Fiction) o;
+				return fiction.name;
+			}
+			else if(o instanceof TextBook){
+				TextBook text=(TextBook) o;
+				return text.subject;
+			}
 		}
 		return null;
 	}
