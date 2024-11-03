168c9ee1cc443fa918936edb747543d415dc9eef
ANUVIND183 <ppanuvind@gmail.com>
2024-11-03 10:09:53 +0530

    Update OldSelection.java

--- DOP/src/oldshelf/OldSelection.java
+++ DOP/src/oldshelf/OldSelection.java
@@ -12,7 +12,18 @@ public class OldSelection {
 	public static String getAgeOrTitle(Object o) {
 		
 		if (o instanceof Book) {
-			
+			if(o instanceof Comic){
+				Comic comic=(Comic) o;
+				return comic.title;
+			}
+			else if(o instanceof Fiction){
+				Fiction fiction=(Fiction) o;
+				return fiction.name;
+			}
+			else if(o instanceof TextBook){
+				TextBook text=(TextBook) o;
+				return text.subject;
+			}
 		}
 		return null;
 	}
@@ -21,6 +32,7 @@ public class OldSelection {
 		
 		// TODO: Write a test code here and execute and text.
 		TextBook t = new TextBook();
-		
+		System.out.println(getAgeOrTitle(t));
+		System.out.println(getAgeOrTitle(null));
 	}
 }
